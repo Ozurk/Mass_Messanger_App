c20e86b3bc7d3672ac9147fc877900ec9664ec8e
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-25 15:03:31 -0400

    Refactored this a little bit using a screen manager

--- main.py
+++ main.py
@@ -11,49 +11,38 @@ from tkinter import messagebox
 from kivy.properties import StringProperty
 from kivy.properties import DictProperty
 from pprint import pprint
-from kivy.event import EventDispatcher
+from kivy.uix.screenmanager import ScreenManager, Screen
 import pandas
+from kivy.lang.builder import Builder
 
 
 def get_dict_from_csv(path_to_csv):
     csv_file = pandas.read_csv(path_to_csv)
     phonebook = csv_file.set_index('Full Name')['Personal Phone'].to_dict()
-    result_dict = DictProperty(phonebook)
+    result_dict = phonebook
 
     return result_dict
 
 
-class MassMessage(BoxLayout):
+class MassMessage(ScreenManager):
     message = StringProperty("")
     phonebook = get_dict_from_csv("data/SF Employee.csv")
     recipients = DictProperty()
 
 
-    def save_and_notify(self, *args):
-        label = self.ids["Input1"]
-        self.message = (label.text)
-        messagebox.showinfo("Notice", f"Message{self.message} will be sent to")
-        print(self.message)
-        print(self.phonebook)
-    
-    def create_user_list(self, *args):
-        userlist = GridLayout(rows=100, cols=5, size_hint_y=None, height=self.height )
-        scrollwidget = ScrollView()
-        scrollwidget.add_widget(userlist)
-        self.add_widget(scrollwidget)
-        for key in self.phonebook:
-            new_button = Button(text=f'{key}', font_size=10)
-            userlist.add_widget(new_button)
-        
-        
+class StartScreen(Screen):
+    pass
+
+class NameSelectorScreen(Screen):
+    tech_names = MassMessage.phonebook.keys()
 
         
-
+    def create_tech_names(self):
+        namegrid = self.ids.NameGrid
+        for tech in self.tech_names:
+            btn = Button(text=f"{tech}", size_hint_y=None, height=30)
+            namegrid.add_widget(btn)
             
-
-            
-
-   
 
 
 class MassMessageApp(App):
@@ -64,3 +53,4 @@ class MassMessageApp(App):
 if __name__ == '__main__':
     app = MassMessageApp()
     app.run()
+
