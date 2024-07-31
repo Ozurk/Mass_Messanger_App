2d63910b19147af42736008a3c213420cd5d5ef8
Hunter Metzger <hunter2000metzger@gmail.com>
2024-07-31 08:17:09 -0400

    Pushing my last commit from last week I belive

--- main.py
+++ main.py
@@ -27,20 +27,50 @@ def get_dict_from_csv(path_to_csv):
 class MassMessage(ScreenManager):
     message = StringProperty("")
     phonebook = get_dict_from_csv("data/SF Employee.csv")
-    recipients = DictProperty()
+    recipients = {}
 
 
 class StartScreen(Screen):
     pass
 
+class ReviewScreen(Screen):
+    def get_recipients(self):
+        return MassMessage.recipients
+
+    
+
+
 class NameSelectorScreen(Screen):
     tech_names = MassMessage.phonebook.keys()
 
-        
+    def change_button_color(self, instance):
+        print(instance.background_color)
+        if instance.background_color == [1, 0, 0, 1]:
+            instance.background_color = (1, 1, 1, 1)
+            MassMessage.recipients.pop(instance.text)
+            
+        else:
+            instance.background_color = (1, 0, 0, 1)
+            MassMessage.recipients[instance.text] = MassMessage.phonebook[instance.text]
+            
+        print(MassMessage.recipients)
+
+    def select_unselect_all(self):
+        for button in self.ids.NameGrid.children:
+            if button.background_color == [1, 0, 0, 1]:
+                button.background_color = (1, 1, 1, 1)
+                MassMessage.recipients.pop(button.text)
+            else:
+                button.background_color = (1, 0, 0, 1)
+                MassMessage.recipients[button.text] = MassMessage.phonebook[button.text]
+        print(MassMessage.recipients)
+            
+
     def create_tech_names(self):
         namegrid = self.ids.NameGrid
         for tech in self.tech_names:
             btn = Button(text=f"{tech}", size_hint_y=None, height=30)
+            btn.bind(on_press=self.change_button_color)
             namegrid.add_widget(btn)
             
 
